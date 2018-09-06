c4032180df4862d6b5931da3aedb5fb82e85181e
Katherine Crowson <crowsonkb@gmail.com>
2018-09-05 22:50:14 -0700

    Use correct sRGB transfer function

--- synthraw/cli.py
+++ synthraw/cli.py
@@ -12,6 +12,11 @@ SRGB_MATRIX = [[3.2406, -1.5372, -0.4986],
                [0.0557, -0.2040, 1.0570]]
 
 
+def srgb_decoding_cctf(arr):
+    """Converts encoded sRGB values to linear RGB values."""
+    return np.where(arr <= 0.04045, arr / 12.92, ((arr + 0.055) / 1.055)**2.4)
+
+
 def main():
     """The main function."""
     ap = argparse.ArgumentParser(description=__doc__)
@@ -20,7 +25,7 @@ def main():
     args = ap.parse_args()
 
     image = np.float32(Image.open(args.input).convert('RGB'))
-    image = np.uint16((image / 255)**2.2 * 65535)
+    image = np.uint16(srgb_decoding_cctf(image / 255) * 65535)
     data = np.zeros(image.shape[:2], np.uint16)
     data[::2, ::2] = image[::2, ::2, 0]
     data[::2, 1::2] = image[::2, 1::2, 1]
